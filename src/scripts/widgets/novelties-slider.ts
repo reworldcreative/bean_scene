31219963b1c7924a6b76046a1cc4bccb4e109b9a
re world <reworldcreative@gmail.com>
2024-12-01 10:41:23 +0200

    add different-section

--- src/scripts/widgets/novelties-slider.ts
+++ src/scripts/widgets/novelties-slider.ts
@@ -2,18 +2,20 @@ import Swiper from 'swiper'
 
 export const swiper = new Swiper('.slider-novelties', {
   slidesPerView: 1.2,
-  spaceBetween: 5,
+  spaceBetween: 10,
 
   breakpoints: {
     350: {
+      slidesPerView: 1.2,
+      spaceBetween: 10,
+    },
+    576: {
       slidesPerView: 2.2,
       spaceBetween: 15,
-      cssMode: true,
     },
     768: {
       slidesPerView: 3.3,
       spaceBetween: 15,
-      cssMode: false,
     },
     1200: {
       slidesPerView: 4,
